69a3c542a3d3737f2a197725c4dc7a615e29009d
cyq333 <820890144@qq.com>
2017-05-09 22:14:34 +0800

    冒泡排序

---
+++ sort/bubbleSort.js
@@ -0,0 +1,16 @@
+/**
+ * Created by admin on 2017/5/7.
+ */
+let bubbleArr = [22,13,34,65,38,92,66,78,99,55];
+for (let i = 0; i < 10; i++) {
+    for (let j = 9; j > i; j--){
+        if (bubbleArr[j] < bubbleArr[j-1]) {
+            let temp = bubbleArr[j];
+            bubbleArr[j] = bubbleArr[j-1]
+            bubbleArr[j-1] = temp;
+        }
+    }
+    console.log(bubbleArr);
+}
+console.log(bubbleArr);
+
