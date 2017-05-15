c389b139ceadf828bb70406fb32168d2578b2da9
cyq333 <820890144@qq.com>
2017-05-15 20:44:45 +0800

    二分查找

---
+++ sort/binarySearch.js
@@ -0,0 +1,19 @@
+binarySearch = (binArray, low, high, key) => {
+    if (low <= high) {
+        mid = Math.floor((low + high) / 2);
+        if (key === binArray[mid]) {
+            return mid;
+        } else if (key < binArray[mid]) {
+            return binarySearch(binArray, low, mid - 1, key);
+        } else if (key > binArray[mid]) {
+            return binarySearch(binArray, mid + 1, high, key);
+        }
+    } else {
+        return -1;
+    }
+}
+
+let tempArray = [22, 13, 34, 65, 38, 92, 66, 78, 99, 55];
+
+let tempResult = binarySearch(tempArray, 0, 9, 78);
+console.log(tempResult);
